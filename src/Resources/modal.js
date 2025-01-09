d23e27aac59d5fa0dcc04c7d3303ef1e2bb6a06a
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2025-01-09 20:48:47 +0100

    upgrade

--- src/Resources/modal.js
+++ src/Resources/modal.js
@@ -14,24 +14,14 @@
                     id: `modal-${Date.now()}-${Math.floor(Math.random() * 1000)}`,
                     url: $(this).attr('href'),
                     size: null,
-                    fullscreen: false,
-                    spinner: true
+                    fullscreen: false
                 };
-
 
                 const settings = $.extend({}, defaultOptions, options);
 
                 if (debug) {
                     console.log('modal create', action, options, settings)
                 }
-
-                $('body').append(`
-                    <div class="text-center" style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background-color: rgba(0, 0, 0, 0.5); z-index: 50000;" id="spinner_` + settings.id + `">
-                      <div class="spinner-border" role="status" style="position: absolute; top:50%">
-                        <span class="sr-only">Loading...</span>
-                      </div>
-                    </div>
-                `);
 
                 $.ajax({
                     url: settings.url,
@@ -73,7 +63,7 @@
                         }
 
                         const modal = $(`
-                        <div class="modal modal-${settings.size}" id="${settings.id}" tabindex="-1" aria-hidden="true" data-bs-backdrop="static">
+                        <div class="modal fade modal-${settings.size}" id="${settings.id}" tabindex="-1" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
                             <div class="modal-dialog ${fullscreen} modal-fullscreen-sm-down">
                                 <div class="modal-content">
                                     <div class="modal-header">
@@ -95,19 +85,11 @@
                         document.getElementById(settings.id).addEventListener('hide.bs.modal', function () {
                             $(this).ajaxmodal('destroy');
                         });
-
-                        if (typeof settings !== 'undefined' && settings.spinner) {
-                            $('#spinner_' + settings.id).remove();
-                        }
                     }
                 })
             } else if (action === 'refresh') {
                 if (debug) {
                     console.log('modal refresh', action, options)
-                }
-
-                if (typeof settings !== 'undefined' && settings.spinner) {
-                    $('#spinner_' + settings.id).remove();
                 }
             } else if (action === 'destroy') {
                 $modal = $(this).closest('.modal');
@@ -118,16 +100,8 @@
                 }
 
                 $modal.remove();
-
-                if (typeof settings !== 'undefined' && settings.spinner) {
-                    $('#spinner_' + settings.id).remove();
-                }
             } else {
                 console.log(`Unknown action: ${action}`);
-
-                if (typeof settings !== 'undefined' && settings.spinner) {
-                    $('#spinner_' + settings.id).remove();
-                }
             }
         });
     };
